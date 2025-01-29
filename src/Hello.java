111a45a130b010c165449ddde442a9fdd44d5b42
OscarChaideza <117925807+OscarChaideza@users.noreply.github.com>
2025-01-29 01:10:09 +0000

    First Commit

--- src/Hello.java
+++ src/Hello.java
@@ -1,1 +1,9 @@
 // Hello World
+
+public class Hello{
+    public static void main(String[] args){
+        //print "Hello World!"
+        //cout << "Hello World!" << endl
+        System.out.println("Hello World!");
+    }
+}
